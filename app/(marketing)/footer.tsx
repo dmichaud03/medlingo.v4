9c5aef066739512d86e66f3cec084c3701185c38
dmichaud03 <85fxllc@gmail.com>
2024-12-21 17:02:31 -0500

    Update footer.tsx

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -7,59 +7,51 @@ export const Footer = () => {
     <div className="hidden h-20 w-full border-t-2 border-slate-200 p-2 lg:block">
       <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
         <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/hr.svg"
-            alt="Croatian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Croatian
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/es.svg"
-            alt="Spanish"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Spanish
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/fr.svg"
-            alt="French"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          French
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/it.svg"
-            alt="Italian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Italian
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/jp.svg"
-            alt="Japanese"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Japanese
-        </Button>
+                    <Image 
+                    src="/anatomy.jpeg" 
+                    alt="Anatomy & Physiology" 
+                    height={32} width={40} 
+                    className="mr-4 rounded-md"/>
+                        Anatomy & Physiology
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                    src="/pharmacology.svg" 
+                    alt="Pharmacology" 
+                    height={32} 
+                    width={40} 
+                    className="mr-4 rounded-md"/>
+                        Fundamentals
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                    src="/pharmacology.svg" 
+                    alt="" 
+                    height={32} 
+                    width={40} 
+                    className="mr-4 rounded-md"/>
+                        Maternity
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                    src="/pharmacology.svg" 
+                    alt="" 
+                    height={32} 
+                    width={40} 
+                    className="mr-4 rounded-md"/>
+                        Pediatrics
+                </Button>
+                
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                    src="/pharmacology.svg" 
+                    alt="" 
+                    height={32} 
+                    width={40} 
+                    className="mr-4 rounded-md"/>
+                        Pharmacology
+                </Button>
+                
       </div>
     </div>
   );
