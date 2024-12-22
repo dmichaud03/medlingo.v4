c3d949cff175b83c038c0ad7d31af32e87351d33
dmichaud03 <85fxllc@gmail.com>
2024-12-22 16:06:38 -0500

    Update route.ts

--- app/api/challengeOptions/[challengeOptionId]/route.ts
+++ app/api/challengeOptions/[challengeOptionId]/route.ts
@@ -1,53 +1,29 @@
-import { eq } from "drizzle-orm";
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 import db from "@/db/drizzle";
 import { challengeOptions } from "@/db/schema";
 import { getIsAdmin } from "@/lib/admin";
 
-export const GET = async (
-  _req: NextRequest,
-  { params }: { params: { challengeOptionId: number } }
-) => {
+export const GET = async () => {
   const isAdmin = getIsAdmin();
   if (!isAdmin) return new NextResponse("Unauthorized.", { status: 401 });
 
-  const data = await db.query.challengeOptions.findFirst({
-    where: eq(challengeOptions.id, params.challengeOptionId),
-  });
+  const data = await db.query.challengeOptions.findMany();
 
   return NextResponse.json(data);
 };
 
-export const PUT = async (
-  req: NextRequest,
-  { params }: { params: { challengeOptionId: number } }
-) => {
+export const POST = async (req: NextRequest) => {
   const isAdmin = getIsAdmin();
   if (!isAdmin) return new NextResponse("Unauthorized.", { status: 401 });
 
   const body = (await req.json()) as typeof challengeOptions.$inferSelect;
+
   const data = await db
-    .update(challengeOptions)
-    .set({
+    .insert(challengeOptions)
+    .values({
       ...body,
     })
-    .where(eq(challengeOptions.id, params.challengeOptionId))
-    .returning();
-
-  return NextResponse.json(data[0]);
-};
-
-export const DELETE = async (
-  _req: NextRequest,
-  { params }: { params: { challengeOptionId: number } }
-) => {
-  const isAdmin = getIsAdmin();
-  if (!isAdmin) return new NextResponse("Unauthorized.", { status: 401 });
-
-  const data = await db
-    .delete(challengeOptions)
-    .where(eq(challengeOptions.id, params.challengeOptionId))
     .returning();
 
   return NextResponse.json(data[0]);
