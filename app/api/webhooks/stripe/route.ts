e03fce0e5034e26c23765bf189d0192dd454cdb3
dmichaud03 <85fxllc@gmail.com>
2024-12-22 13:14:45 -0500

    Update route.ts

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: NextRequest) {
     event = stripe.webhooks.constructEvent(
       body,
       signature,
-      STRIPE_WEBHOOK_SECRET
+      process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (error: unknown) {
     return new NextResponse(`Webhook error ${JSON.stringify(error)}`, {
