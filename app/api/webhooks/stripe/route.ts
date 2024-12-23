47d20851ac7946426f72525c1ed34344ff14e378
dmichaud03 <85fxllc@gmail.com>
2024-12-22 19:18:36 -0500

    Update route.ts

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,11 +1,8 @@
-
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-
-import { eq } from "drizzle-orm";
 import db from "@/db/drizzle";
-
 import { userSubscription } from "@/db/schema";
 import { stripe } from "@/lib/stripe";  
 
