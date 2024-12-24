06579eea99703e2cb30b1ae4f5bb39b353da172e
dmichaud03 <85fxllc@gmail.com>
2024-12-23 19:27:05 -0500

    Update header.tsx

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -21,7 +21,7 @@ export const Header = () => {
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
         <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
-          <Image src="hero.png" alt="Hero" height={40} width={40} />
+          <Image src="hero2.png" alt="Hero" height={40} width={40} />
 
           <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
             Medlingo
